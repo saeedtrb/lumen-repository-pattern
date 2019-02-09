e812fd3c7f3be0738380c141a97ec6f220fc03ba
saeed torabi <saeedtrb@yahoo.com>
2019-02-09 15:09:41 +0330

    add app config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,6 +27,8 @@ $app = new Laravel\Lumen\Application(
 
 // $app->withEloquent();
 
+$app->configure('app');
+
 /*
 |--------------------------------------------------------------------------
 | Register Container Bindings
